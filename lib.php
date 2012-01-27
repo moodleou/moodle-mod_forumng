moodleou/moodle-mod_ouwiki

oublog, ouwiki, forumng, dataplus, ouelluminate not hidden on course page if no view capability fixes #2118

lib.php
@@ -575,3 +575,15 @@ function ouwiki_grade_item_delete($ouwiki) {
     return grade_update('mod/ouwiki', $ouwiki->course, 'mod',
         'ouwiki', $ouwiki->id, 0, null, array('deleted' => 1));
 }
+
+/**
+ * Sets the module uservisible to false if the user has not got the view capability
+ * @param cm_info $cm
+ */
+function ouwiki_cm_info_dynamic(cm_info $cm) {
+    if (!has_capability('mod/ouwiki:view',
+            get_context_instance(CONTEXT_MODULE,$cm->id))) {
+        $cm->uservisible = false;
+        $cm->set_available(false);
+    }
+}

moodleou/moodle-mod_forumng

lib.php
@@ -381,3 +381,15 @@ function mod_forumng_cm_info_view(cm_info $cm) {
         }
     }
 }
+
+/**
+ * Sets the module uservisible to false if the user has not got the view capability
+ * @param cm_info $cm
+ */
+function mod_forumng_cm_info_dynamic(cm_info $cm) {
+    if (!has_capability('mod/forumng:view',
+            get_context_instance(CONTEXT_MODULE,$cm->id))) {
+        $cm->uservisible = false;
+        $cm->set_available(false);
+    }
+}