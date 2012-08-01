dbde7137b4e2b20d1ab7121511750ef0c31c38bf
Ray Guo <r.guo@open.ac.uk>
2012-08-01 15:56:11 +0100

    #4252 Forumng cron fails attempting to archive/delete discussions due to calling non-exist function get_attachment_folder()

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version  = 2012071200;
+$module->version  = 2012080100;
 $module->requires = 2011120100;
 $module->cron     = 60;
 
