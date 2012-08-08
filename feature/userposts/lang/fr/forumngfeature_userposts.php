e07e6425dc7bdd9d0e1e771aeceaf0694ef203f8
sam marshall <s.marshall@open.ac.uk>
2012-08-08 13:26:18 +0100

    ForumNG: Add French language support #4321
    Translator: Luiggi Sansonetti

---
+++ feature/userposts/lang/fr/forumngfeature_userposts.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings.
+ * @package forumngfeature
+ * @subpackage userposts
+ * @copyright 2011 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['viewpostsbyuser'] = 'Afficher les messages par utilisateur';
+$string['userposts'] = 'Messages de l\'utilisateur';
+$string['showallpostsby'] = 'Afficher tous les messages de {$a}';
+$string['allpostsby'] = 'Tous les message de : {$a}';
+$string['notforsharedforum'] = 'Non pris en charge pour les forums partag&eacute;s';
+$string['replyin'] = 'R&eacute;ponse &agrave; la discussion : {$a}';
+$string['userposts:view'] = 'Afficher la liste des messages de l\'utilisateur';
+$string['pluginname'] = 'Affichage des messages par utilisateur';
+$string['action'] = 'Action';
