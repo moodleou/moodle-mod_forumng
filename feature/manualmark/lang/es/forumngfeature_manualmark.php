1dde773960bae4403004db7b249ce7814b8c75ba
Alvaro Maceda <alvaro@alvaromaceda.es>
2013-08-04 23:45:44 +0200

    Translation to Spanish

---
+++ feature/manualmark/lang/es/forumngfeature_manualmark.php
@@ -0,0 +1,33 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Lang strings.
+ * @package forumngfeature
+ * @subpackage manualmark
+ * @copyright 2011 The Open University
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['manualmark_auto'] = 'Marcar automáticamente los temas como leídos';
+$string['manualmark_manual'] = 'Marcar los temas como leídos de forma manual';
+$string['manualmark_change'] = 'Cambiar';
+
+$string['manualmark_change_help'] = '&lsquo;Marcar automáticamente los temas como leídos&rsquo; marca todas las entradas de un tema como leídas al leer cualquier entrada de ese tema.
+
+&lsquo;Marcar los temas como leídos de forma manual&rsquo; deja los posts como no leídos hasta que pulses en el botón &lsquo;Marcar tema como leído&rsquo;.
+
+Esta opción se aplica a todos los foros del sistema.';
+$string['pluginname'] = 'Option to manually mark discussions read';
