c4458368a22b57790625e5bbbacf83aef86ed90e
Jason Platts <j.platts@open.ac.uk>
2013-10-24 16:23:50 +0100

    OUWiki Forumng OUBlog: Update version requires to 2.5 [M2.5] #8640

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 
 $module->version  = 2013102200;
-$module->requires = 2011120100;
+$module->requires = 2013040500;
 $module->cron     = 60;
 
 // Note: This should be replaced with standard ->maturity and other options.
