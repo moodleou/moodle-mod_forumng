0ff24ca23dbad1f11003c5e5063594276848606f
Jason Platts <j.platts@open.ac.uk>
2014-02-06 10:30:20 +0000

    ForumNG: Discussion Options page throws webpage error #9755

--- form.js
+++ form.js
@@ -57,42 +57,44 @@ M.mod_forumng_form = {
             var submit = t.Y.one('#id_submitbutton');
             var savedraft = t.Y.one('#id_savedraft');
             var textarea = t.Y.one('#id_message');
-            var sourcetext = textarea.get('value');
-            if (textarea.getStyle('display') == 'none' && window.tinyMCE &&
-                    tinyMCE.activeEditor) {
-                sourcetext = tinyMCE.activeEditor.getBody().innerHTML;
-            }
+            if (textarea) {
+                var sourcetext = textarea.get('value');
+                if (textarea.getStyle('display') == 'none' && window.tinyMCE &&
+                        tinyMCE.activeEditor) {
+                    sourcetext = tinyMCE.activeEditor.getBody().innerHTML;
+                }
 
-            // Get rid of tags and nbsp as literal or entity, then trim.
-            var mungevalue = sourcetext.replace(/<.*?>/g, '').replace(
-                /&(nbsp|#160|#xa0);/g, '') . replace(
-                    new RegExp(String.fromCharCode(160), 'g'), ' ') .
-                replace(/\s+/, ' ') . trim();
+                // Get rid of tags and nbsp as literal or entity, then trim.
+                var mungevalue = sourcetext.replace(/<.*?>/g, '').replace(
+                    /&(nbsp|#160|#xa0);/g, '') . replace(
+                        new RegExp(String.fromCharCode(160), 'g'), ' ') .
+                    replace(/\s+/, ' ') . trim();
 
-            // Allow an image even if no text.
-            if (sourcetext.indexOf('<img ') != -1) {
-                mungevalue = 'gotimage';
-            }
+                // Allow an image even if no text.
+                if (sourcetext.indexOf('<img ') != -1) {
+                    mungevalue = 'gotimage';
+                }
 
-            // We will disable the button if there is no text.
-            var disable = mungevalue === '';
+                // We will disable the button if there is no text.
+                var disable = mungevalue === '';
 
-            // When editing discussion first post, subject must also be not blank.
-            if (!disable && t.Y.one('#fitem_id_subject.required')) {
-                if (t.Y.one('#id_subject').get('value').trim() === '') {
+                // When editing discussion first post, subject must also be not blank.
+                if (!disable && t.Y.one('#fitem_id_subject.required')) {
+                    if (t.Y.one('#id_subject').get('value').trim() === '') {
+                        disable = true;
+                    }
+                }
+
+                // When the editing time limit has expired, you cannot save.
+                if (timelimit && get_unix_time() > timelimit-30) {
                     disable = true;
                 }
-            }
 
-            // When the editing time limit has expired, you cannot save.
-            if (timelimit && get_unix_time() > timelimit-30) {
-                disable = true;
-            }
-
-            // Disable saving and also drafts.
-            submit.set('disabled', disable);
-            if (savedraft) {
-                savedraft.set('disabled', disable);
+                // Disable saving and also drafts.
+                submit.set('disabled', disable);
+                if (savedraft) {
+                    savedraft.set('disabled', disable);
+                }
             }
         }, 250);
     }
