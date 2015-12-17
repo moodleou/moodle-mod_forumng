59d9d36c307981772623de70e0dc5e54ddeeda87
sam marshall <s.marshall@open.ac.uk>
2015-12-17 10:18:32 +0000

    Update to current OU live version

--- module.js
+++ module.js
@@ -1260,10 +1260,10 @@ M.mod_forumng = {
         if (newpost.one('.forumng-ratings-standard')) {
             if (M.core_rating && M.core_rating.init) {
                 M.core_rating.init(this.Y);
-            } else if (M.local_themeextras && M.local_themeextras.rating.init) {
+            } else if (M.theme_ou && M.theme_ou.rating.init) {
                 // OU only code for custom ratings.
-                M.local_themeextras.rating.init(link.postid);
-                M.local_themeextras.rating.init_count_popup(link.postid);
+                M.theme_ou.rating.init(link.postid);
+                M.theme_ou.rating.init_count_popup(link.postid);
             }
         }
 
--- version.php
+++ version.php
@@ -22,8 +22,8 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version = 2015082103;
+$plugin->version = 2015082101;
 $plugin->requires = 2014111000;
 $plugin->component = 'mod_forumng';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.9 r1';
+$plugin->release = '2.8 r1';
