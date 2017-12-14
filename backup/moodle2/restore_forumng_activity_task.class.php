fd54013013e518c3bfa0a91fbe958999fd9314f8
hieuvu <hieu.vuvan@harveynash.vn>
2017-12-14 17:04:11 +0700

    IPUD: SC/VLE: Support in-page discussions [SC2] #214906

--- backup/moodle2/restore_forumng_activity_task.class.php
+++ backup/moodle2/restore_forumng_activity_task.class.php
@@ -58,8 +58,8 @@ class restore_forumng_activity_task extends restore_activity_task {
 
         $contents[] = new restore_decode_content('forumng', array('intro'));
         $contents[] = new restore_decode_content('forumng', array('introduction'));
-        $contents[] = new restore_decode_content('forumng_posts', array('message'));
-        $contents[] = new restore_decode_content('forumng_discussions', array('ipudloc'));
+        $contents[] = new restore_decode_content('forumng_posts', array('message'), 'forumng_post');
+        $contents[] = new restore_decode_content('forumng_discussions', array('ipudloc'), 'forumng_discussion');
         return $contents;
     }
 
