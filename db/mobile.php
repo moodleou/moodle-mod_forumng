19516663d0a36d18d0c15eac3858c957a26fba07
sam marshall <s.marshall@open.ac.uk>
2020-11-19 11:54:47 +0000

    Mobile: Dark mode styling issues (Forums) #429826

--- db/mobile.php
+++ db/mobile.php
@@ -38,8 +38,8 @@ $addons = [
                 'downloadbutton' => false,
                 'displayprefetch' => false,
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2020111803',
-                    'version' => 2020111803
+                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2020111900',
+                    'version' => 2020111900
                 ],
                 'init' => 'mobile_forumng_init'
             ]
