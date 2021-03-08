b23f911e352fe5c35d3fe7b236cef44d880f4532
NhanVo <Nhan.VoHuynhTinh1@nashtechglobal.com>
2021-03-08 14:56:35 +0700

    Mobile: Forum - Deleted discussion's name displays incorrectly #446257

--- db/mobile.php
+++ db/mobile.php
@@ -38,8 +38,8 @@ $addons = [
                 'downloadbutton' => false,
                 'displayprefetch' => false,
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2020120300',
-                    'version' => 2020120300
+                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2021030800',
+                    'version' => 2021030800
                 ],
                 'init' => 'mobile_forumng_init'
             ]
