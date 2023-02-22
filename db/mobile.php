154aaa3f5594c99ac3c7db2403076678747a03b3
Quy Phan Quang <qpq2@open.ac.uk>
2023-02-22 13:25:09 +0700

    Studyapp 4.0.1: Dark mode: Info icon to change to white #641919

--- db/mobile.php
+++ db/mobile.php
@@ -38,8 +38,8 @@ $addons = [
                 'downloadbutton' => false,
                 'displayprefetch' => false,
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2022011601',
-                    'version' => 2022011601
+                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2023022200',
+                    'version' => 2023022200
                 ],
                 'init' => 'mobile_forumng_init'
             ]
