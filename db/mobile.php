dcc0b3e366b033cd97cdd74afb7743e4957b4de6
tai.letan <tlt84@open.ac.uk>
2023-10-31 10:59:44 +0700

    Studyapp 4.2.0.2/4.0.2.5: Forums icons styling fixes #699055

--- db/mobile.php
+++ db/mobile.php
@@ -38,8 +38,8 @@ $addons = [
                 'downloadbutton' => false,
                 'displayprefetch' => false,
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2023091200',
-                    'version' => 2023091200
+                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2023110100',
+                    'version' => 2023110100
                 ],
                 'init' => 'mobile_forumng_init'
             ]
