951cd4aa91ed3550d521cc0bef7f7e11c5898f15
Quy Phan Quang <qpq2@open.ac.uk>
2024-01-02 09:26:11 +0700

    Studyapp 4.0.2.5/4.2.0.4 - Forum: Discussion, reply alignment #735233

--- db/mobile.php
+++ db/mobile.php
@@ -38,8 +38,8 @@ $addons = [
                 'downloadbutton' => false,
                 'displayprefetch' => false,
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2023122800',
-                    'version' => 2023122800
+                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2024010200',
+                    'version' => 2024010200
                 ],
                 'init' => 'mobile_forumng_init'
             ]
