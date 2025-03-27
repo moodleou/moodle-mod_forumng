8a1a908532b929518bee79ace8d8759b4b61a711
toanlamt <toanlamt1@nashtechglobal.com>
2025-03-27 16:44:51 +0700

    Studyapp 4.4.1.5: Font size of subject title is inconsistence #869529

--- db/mobile.php
+++ db/mobile.php
@@ -38,8 +38,8 @@ $addons = [
                 'downloadbutton' => false,
                 'displayprefetch' => false,
                 'styles' => [
-                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2025020700',
-                    'version' => 2025020700
+                    'url' => $CFG->wwwroot . '/mod/forumng/mobile_css.php?v=2025032700',
+                    'version' => 2025032700
                 ],
                 'init' => 'mobile_forumng_init'
             ]
