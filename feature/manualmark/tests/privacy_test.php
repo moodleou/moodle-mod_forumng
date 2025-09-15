849f40b34df1511c769bbed0f7374e4a17ae7492
Ewan Westcott Graham <ewg27@open.ac.uk>
2025-09-15 12:10:40 +0100

    PHPUnit: Update namespaces for contradicting classes #938272

--- feature/manualmark/tests/privacy_test.php
+++ feature/manualmark/tests/privacy_test.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace mod_forumng;
+namespace forumngfeature_manualmark;
 
 /**
  * Test data provider for forumngfeature_manualmark.
